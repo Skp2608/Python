674ad99733711df08f0840e3a111cca65d63f220
ravi kumar <56681238+ravi1232015@users.noreply.github.com>
2020-10-01 10:41:30 +0530

    update

--- leap_year.py
+++ leap_year.py
@@ -1,8 +1,8 @@
 # program to check leap year
 y=int(input("Enter a year :"))
 if y%4==0 and y%100!=0:
-    print("Given year is leap year")
+    print("year is leap year")
 elif y%400==0:
-    print("Given year is leap year")
+    print("year is leap year")
 else:
-    print("Given year is not leap year")
+    print("year is not leap year")
